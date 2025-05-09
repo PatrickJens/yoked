6bf543fa2f0c7238a7a6faed90f7732560f33ce5
Patrick Cannell <pcannell14@gmail.com>
2025-05-09 15:12:39 -0500

    HttpContext

--- yoked/Program.cs
+++ yoked/Program.cs
@@ -7,9 +7,25 @@ namespace yoked
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
             /* Builder Adds Injected Services */
-            String test_git = "testXXXX";
-            String envVar = Environment.GetEnvironmentVariable("home_message");
-            app.MapGet("/", () => envVar);
+
+            
+            app.Run( async ( HttpContext context) =>
+            {
+                StreamReader reader = new StreamReader(context.Request.Body);
+                String body = await reader.ReadToEndAsync();
+                context.Response.Headers["Content-type"] = "text/html";
+                if ( context.Request.Method == "POST" && context.Request.Query.ContainsKey("id"))
+                {
+                    String id = context.Request.Query["id"];
+                    await context.Response.WriteAsync($"<p>{id}<p>");
+                }
+                String path = context.Request.Path;
+                String method = context.Request.Method;
+                await context.Response.WriteAsync($"<p>{path}<p>");
+                await context.Response.WriteAsync($"<p>{method}<p>");
+                
+            });
+
             app.Run();
         }
     }
