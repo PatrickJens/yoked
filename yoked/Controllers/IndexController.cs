cee1c5fc43c294e99fa0abbf5dc5b7b3d53667d9
Patrick Cannell <pcannell14@gmail.com>
2025-05-12 16:06:23 -0500

    index controller

---
+++ yoked/Controllers/IndexController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace yoked.Controllers
+{
+    public class IndexController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+    }
+}
--- yoked/Program.cs
+++ yoked/Program.cs
@@ -7,8 +7,11 @@ namespace yoked
             var builder = WebApplication.CreateBuilder(args);
             /* Builder Adds Injected Services */
             builder.Services.AddControllers();
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
-            app.MapControllers();          
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.MapControllers();
             app.Run();
         }
     }
